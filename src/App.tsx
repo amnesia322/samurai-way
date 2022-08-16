ad3188e27e2262db260fd40ee868c6a261be319a
amnesia322 <dev.amnesia322@gmail.com>
2022-08-16 17:06:09 +0300

    lesson-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
+
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' alt='header-logo'/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Message</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+                        alt=""/>
+                </div>
+                <div> ava + description</div>
+                <div> My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
-
 
 
 export default App;
