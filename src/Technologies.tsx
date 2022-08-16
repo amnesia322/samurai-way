70639451567d026d0cd962e6b73a88b7f8be0ceb
amnesia322 <dev.amnesia322@gmail.com>
2022-08-16 16:24:44 +0300

    lesson-09

---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Technologies() {
+    return (
+        <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>JS</li>
+            <li>REACT</li>
+        </ul>
+    );
+}
+
+export default Technologies
