440ec02ab17248d565d4659ac462d705109a0483
amnesia322 <dev.amnesia322@gmail.com>
2022-08-16 16:29:25 +0300

    lesson-10

---
+++ src/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>JS</li>
+            <li>REACT</li>
+        </ul>
+    );
+}
+
+export default Footer
