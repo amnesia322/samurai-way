ce6eec7591e647a7876e4baf144048597d05ca1a
amnesia322 <dev.amnesia322@gmail.com>
2022-08-25 22:32:19 +0300

    refactoring

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}><NavLink to='/dialogs/1'>Vasya</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/2'>Rogoz</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/3'>Berya</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/4'>Ivan</NavLink></div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>how ary u?</div>
+                <div className={s.message}>vot eto project!</div>
+            </div>
         </div>
     )
 }
