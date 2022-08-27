f12e6fcba3369df672ec42d4e7e570f08416d673
amnesia322 <dev.amnesia322@gmail.com>
2022-08-27 17:48:41 +0300

    refactoring Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogItemType = {
+    name: string,
+    id: number
+}
+
+type MessageType = {
+    message: string
+}
+
+const DialogItem = (props: DialogItemType) => {
+    return <div className={s.dialog}><NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink></div>
+}
+
+const Message = (props: MessageType) => {
+    return <div className={s.message}>{props.message}</div>
+}
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}><NavLink to='/dialogs/1'>Vasya</NavLink></div>
-                <div className={s.dialog}><NavLink to='/dialogs/2'>Rogoz</NavLink></div>
-                <div className={s.dialog}><NavLink to='/dialogs/3'>Berya</NavLink></div>
-                <div className={s.dialog}><NavLink to='/dialogs/4'>Ivan</NavLink></div>
+                <DialogItem id={1} name='Dimych'/>
+                <DialogItem id={2} name='Daniil'/>
+                <DialogItem id={3} name='Rogoz'/>
+                <DialogItem id={4} name='Vitaliy'/>
+                <DialogItem id={5} name='Ivan'/>
+                <DialogItem id={6} name='Valera'/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>hi</div>
-                <div className={s.message}>how ary u?</div>
-                <div className={s.message}>vot eto project!</div>
+                <Message message='hi' />
+                <Message message='how are u?' />
+                <Message message='crazy project!' />
+                <Message message='vozmite menya rabotat)' />
             </div>
         </div>
     )
