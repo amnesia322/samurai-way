146f299ba21df49fce7999dc23035ecb859fcb0f
amnesia322 <dev.amnesia322@gmail.com>
2022-10-25 16:21:13 +0300

    refactoring Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,29 +7,44 @@ const Users = (props: UsersPropsType) => {
 
     if (props.users.length === 0) {
         props.setUsers([
-            {id: v1(), photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
-                followed: true, fullName: 'Dmitriy', status: 'Im boss!', location: {country: 'Belarus', city: 'Minsk'}},
-            {id: v1(), photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
-                followed: true, fullName: 'Vitaliy', status: 'Whats up?!', location: {country: 'Russia', city: 'Moscow'}},
-            {id: v1(), photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
-                followed: false, fullName: 'amnesia', status: 'find for job', location: {country: 'Russia', city: 'Voronezh'}},
+            {
+                id: v1(), photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+                followed: true, fullName: 'Dmitriy', status: 'Im boss!', location: {country: 'Belarus', city: 'Minsk'}
+            },
+            {
+                id: v1(), photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+                followed: true, fullName: 'Vitaliy', status: 'Whats up?!', location: {country: 'Russia', city: 'Moscow'}
+            },
+            {
+                id: v1(),
+                photoURL: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+                followed: false,
+                fullName: 'amnesia',
+                status: 'find for job',
+                location: {country: 'Russia', city: 'Voronezh'}
+            },
         ])
     }
     //
 
     return (
         <div>
-            {props.users.map(u => <div key={u.id}>
+            {props.users.map(u => {
+                    const onFollowHandler = () => props.follow(u.id);
+                    const onUnfollowHandler = () => props.unfollow(u.id);
+
+                    return <div key={u.id}>
+
                 <span>
                     <div>
                         <img src={u.photoURL} className={s.avatar} alt='userAvatar'/>
                     </div>
                     <div>
-                        {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                            : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                        {u.followed ? <button onClick={onUnfollowHandler}>Unfollow</button>
+                            : <button onClick={onFollowHandler}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                        <span>
                     <span>
                         <div>{u.fullName}</div>
                         <div>{u.status}</div>
@@ -40,7 +55,9 @@ const Users = (props: UsersPropsType) => {
 
                     </span>
                 </span>
-            </div>)}
+                    </div>
+                }
+            )}
         </div>
     );
 };
